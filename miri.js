8ab9599cd3f4d88808b33cf1cf4d2569e7094c7e
angelestun <angelestun3@gmail.com>
2024-11-29 23:02:02 -0600

    cambios server endpoint

--- miri.js
+++ miri.js
@@ -18,7 +18,7 @@ app.head('/health', (req, res) => {
 
 
 app.use(cors({
-    origin: ['https://estravagant-frond.vercel.app/', 'http://localhost:5173'],
+    origin: ['https://extravagant-style.vercel.app/', 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
